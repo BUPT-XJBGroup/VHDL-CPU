45082bbb960290e171af5e7b3994d8a60e1f7d93
9646516 <zyq855@gmail.com>
2019-03-28 00:01:09 +0800

    Fix silly mistakes

--- VHDL-CPU/CPU.vhd
+++ VHDL-CPU/CPU.vhd
@@ -11,17 +11,17 @@ entity CPU is
 		-- 7654 => 3210
 		W : in std_logic_vector(2 downto 0);
 		-- 321 => 210
-		DRW,PCINC,LPC,LAR,PCADD,ARINC,SELCTL,MEMW,STOP,LIR,LDZ,LDC,CIN,M,ABUS,SBUS,MBUS,SHORT,LONG :out std_logic;
-		S : in std_logic_vector(3 downto 0);
+		DRW,PCINC,LPC,LAR,PCADD,ARINC,SELCTL,MEMW,STOP,LIR,LDZ,LDC,CIN,M,ABUS,SBUS,MBUS,SHORT,LONG : out std_logic;
+		S : out std_logic_vector(3 downto 0);
 		-- 3210 => 3210
-		SEL01 : in std_logic_vector(1 downto 0);
+		SEL01 : out std_logic_vector(1 downto 0);
 		-- 10 => 10
-		SEL23 : in std_logic_vector(1 downto 0)
+		SEL23 : out std_logic_vector(1 downto 0)
 		-- 32 => 10
 	);
 end CPU;
 
 architecture arc of CPU is
-begin	
+begin
 
 end arc;
