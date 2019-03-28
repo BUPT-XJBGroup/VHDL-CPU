9a2c27b30fb5a990294e622cf6bf720a03d0f757
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2019-03-28 21:55:07 +0800

    Update CPU.vhd

--- VHDL-CPU/CPU.vhd
+++ VHDL-CPU/CPU.vhd
@@ -60,9 +60,11 @@ begin
 
 	process (CLR, T3)
 	begin
-		if (CLR'event and CLR = '0') then
+		-- 任何时候按下CLR, 都会返回
+		if (CLR = '0') then
 			ST0_REG <= '0';
 			STOP_REG_REG <= '1';
+		-- 如果到节拍电位T3下降沿，ST0_REG |= SST0
 		else if (T3'event and T3 = '0') then
 			if (SST0 = '1') then
 				ST0_REG <= '1';
@@ -72,30 +74,59 @@ begin
 	
 	process (SWCBA, IRH, W1, W2, W3, ST0, C, Z)
 	begin
+	
+		-- 初始化 和 状态参数
 		SHORT <= '0';
 		LONG <= '0';
-		CIN <= '0';
-		SELCTL <= '0';
+		-- 设置STOP
+		STOP_REG <= '1';
+		-- 设置ST0标志
+		SST0 <= '0';
+		
+		-- ALU
 		ABUS <= '0';
+		M <= '0';
+		CIN <= '0';
+		S <= "0000";
+		ARINC <= '0';
+		-- 保存Z标志
+		LDZ <= '0';
+		-- 保存C标志
+		LDC <= '0';		
+	
 		SBUS <= '0';
 		MBUS <= '0';
-		M <= '0';
-		S <= "0000";
+		
+		-- 控制台操作标志
+		SELCTL <= '0';
+		
+		-- RD1~RD0
 		SEL3 <= '0';
 		SEL2 <= '0';
+		
+		-- RS1~RS0
 		SEL1 <= '0';
 		SEL0 <= '0';
+		
+		-- 送指令寄存器标志
 		LIR <= '0';
-		MEMW <= '0';
+		-- 送地址寄存器标志
 		LAR <= '0';
-		ARINC <= '0';
+		-- 送程序计数器标志
 		LPC <= '0';
-		LDZ <= '0';
-		LDC <= '0';
-		STOP_REG <= '0';
-		PCINC <= '0';
-		SST0 <= '0';
+
+		
+		-- (~R)/W
+		MEMW <= '0';
+		DRW <= '0'
+		
+		-- 程序计数器自增标志
+		PCINC <= '0';		
+		-- 程序计数器增量标志
 		PCADD <= '0';
+		
+		
+		
 		case SWCBA is
 			when "000" =>  --执行程序
 				case ST0 is
@@ -104,25 +135,40 @@ begin
 						LPC <= W1;
 						SBUS <= W1;
 						SST0 <= W1;
+						
 						SHORT <= W1;
-						STOP_REG <= '0';
+						-- STOP_REG <= '0';
 					when '1' =>
 						case IRH is
+							when "0000" =>  -- NOP
+								
+								-- 设定PC
+								LIR <= W1;
+								PCINC <= W1;
+								
+								-- 短周期
+								SHORT <= W1;
 							when "0001" =>  --ADD ()
 								
 								-- 设定PC
 								LIR <= W1;
 								PCINC <= W1;
 								
+								-- 短周期
+								SHORT <= W1;
+								
+								-- ABUS = W1
+								ABUS <= W1;
+								CIN <= W1;
+								
 								-- 选择加法
 								-- 选择算术运算, M已经被初始化为0
 								S <= "1001";
 								
 								-- 加法操作
-								ABUS <= W2;
-								DRW <= W2;
-								LDZ <= W2;
-								LDC <= W2;
+								DRW <= W1;
+								LDZ <= W1;
+								LDC <= W1;
 							
 							when "0010" =>  -- SUB ()
 								
@@ -130,158 +176,228 @@ begin
 								LIR <= W1;
 								PCINC <= W1;
 								
+								-- 短周期
+								SHORT <= W1;
+								
 								-- 选择算术运算, 选择减法
 								-- M已经被初始化为0
 								S <= "0110";
+								-- TODO: CIN呢?
 								
 								-- 减法操作
-								ABUS <= W2;
-								DRW <= W2;
-								LDZ <= W2;
-								LDC <= W2;
+								ABUS <= W1;
+								DRW <= W1;
+								LDZ <= W1;
+								LDC <= W1;
 							when "0011" =>  -- AND ()
 								
 								-- 设定PC
 								LIR <= W1;
 								PCINC <= W1;
 								
+								-- 短周期
+								SHORT <= W1;
+								
 								-- 选择逻辑运算, 与运算
-								M <= W2;
+								M <= W1;
 								S <= "1011";
 								
-								ABUS <= W2;
-								DRW <= W2;
-								LDZ <= W2;
+								ABUS <= W1;
+								DRW <= W1;
+								LDZ <= W1;
 							when "0100" =>  -- INC ()
 								
 								-- 设定PC
 								LIR <= W1;
 								PCINC <= W1;
 								
+								-- 短周期
+								SHORT <= W1;
+								
 								-- 选择算术运算, 与运算
 								-- M已经被初始化为0
 								S <= "0000";
 								
-								ABUS <= W2;
-								DRW <= W2;
-								LDZ <= W2;
-								LDC <= W2;
+								ABUS <= W1;
+								DRW <= W1;
+								LDZ <= W1;
+								LDC <= W1;
 								
 							when "0101" =>  -- LD
 								
-								-- 设定PC
-								LIR <= W1;
-								PCINC <= W1;
 								
 								-- 选择算术运算，传送B（保留原值）
-								M <= W2;
+								M <= W1;
 								S <= "1010";
 								
-								ABUS <= W2;
-								LAR <= W2;
-								
-								-- 延长周期
-								LONG <= W2;
-								
-								MBUS <= W3;
-								DRW <= W3;
-							when "0110" =>  -- ST
+								ABUS <= W1;
+								LAR <= W1;
 								
 								-- 设定PC
-								LIR <= W1;
-								PCINC <= W1;
+								LIR <= W2;
+								PCINC <= W2;
+								
+								MBUS <= W2;
+								DRW <= W2;
+							when "0110" =>  -- ST
+								
 								
 								-- 设定...
-								M <= W2 or W3;
-								S <= '1' & W2 & '1' & W2;
+								M <= W1 or W2;
+								S <= '1' & W1 & '1' & W1;
 								
-								ABUS <= W2 or W3;
-								LAR <= W2;
 								
-								-- 延长周期
-								LONG <= W2;
+								ABUS <= W1 or W2;
+								LAR <= W1;
 								
-								MEMW <= W3;
+								MEMW <= W2;
+								-- 设定PC
+								LIR <= W2;
+								PCINC <= W2;
+								
 							
 							when "0111" =>  -- JC
 								
 								-- 设定PC
-								LIR <= W1;
-								PCINC <= W1;
+								LIR <= (W1 and (not C)) or (W2 or C);
+								PCINC <= (W1 and (not C)) or (W2 or C);
 								
-								PCADD <= C and W2;
+								PCADD <= C and W1;
+								SHORT <= W1 and (not C);
 							
 							when "1000" =>  -- JZ
 								
 								-- 设定PC
-								LIR <= W1;
-								PCINC <= W1;
+								LIR <= (W1 and (not Z)) or (W2 or Z);
+								PCINC <= (W1 and (not Z)) or (W2 or Z);
 								
-								PCADD <= Z and W2;
+								PCADD <= Z and W1;
+								SHORT <= W1 and (not Z);
 								
 							when "1001" =>  -- JMP
 								
-								-- 设定PC
-								LIR <= W1;
-								PCINC <= W1;
-								
 								-- 设定算术运算
-								M <= W2;
+								M <= W1;
 								S <= "1111";
 								
-								ABUS <= W2;
-								LPC <= W2;
+								ABUS <= W1;
+								LPC <= W1;
+								
+								-- 设定PC
+								LIR <= W2;
+								PCINC <= W2;
 								
 							when "1010" =>  -- OUT
 								
+								-- 设定PC
+								LIR <= W1;
+								PCINC <= W1;
+								
+								-- 短周期
+								SHORT <= W1;
+								
 								-- 设定算术运算
-								M <= W2;
+								M <= W1;
 								S <= "1010";
-								ABUS <= W2;
-							when others =>  -- 空指令
+								ABUS <= W1;
+								
+							when "1110" =>  -- STP
+							
+								STOP_REG <= W1;
+							
+							when others =>  -- 公操作
 								
 								-- 设定PC
 								LIR <= W1;
 								PCINC <= W1;
+								
+								-- TODO: SHORT呢?
 						end case;
 					when others =>
 						-- 不可能到这吧?
 				end case;
 			when "001" =>
 				
-				SELCTL<=W1;
-				SHORT<=W1;
-				SBUS<=W1;
-				STOP_REG<=W1;
-				SST0<=W1;
-				LAR<=W1 and (not ST0);
-				ARINC<=W1 and ST0;
-				MEMW<=W1 and ST0;
+				-- SBUS = (ST0=0 or ST0=1) and W1 
+				SBUS <= W1;
+				-- STOP = (ST0=0 or ST0=1) and W1
+				STOP_REG <= W1;
+				-- SHORT = (ST0=0 or ST0=1) and W1
+				SHORT <= W1;
+				-- SELCTL = (ST0=0 or ST0=1) and W1
+				SELCTL <= W1;
+				
+				-- LAR = (ST0=0) and W1
+				LAR <= W1 and (not ST0);
+				
+				-- LAR = (ST0=1) and W1
+				ARINC <= W1 and ST0;
+				-- MEMW = (ST0=1) and W1
+				MEMW <= W1 and ST0;
+				
+				-- TODO: 存疑
+				SST0 <= W1;
+				
 			when "010" =>
-				SELCTL<=W1;
+				
+				-- SHORT = (ST0=0 or ST0=1) and W1
 				SHORT<=W1;
-				SBUS<=W1 and (not ST0);
-				MBUS<=W1 and ST0;
+				-- SELCTL = (ST0=0 or ST0=1) and W1
+				SELCTL <= W1;
+				-- STOP = (ST0=0 or ST0=1) and W1
 				STOP_REG<=W1;
-				SST0<=W1;
+				
+				-- SBUS = (ST0=0) and W1
+				SBUS<=W1 and (not ST0);
+				-- LAR = (ST0=0) and W1
 				LAR<=W1 and (not ST0);
+				
+				-- MBUS = (ST0=1) and W1
+				MBUS<=W1 and ST0;
+				-- ARINC = (ST0=1) and W1
 				ARINC<=W1 and ST0;
+				
+				-- TODO: 存疑
+				SST0<=W1;
 			when "011" =>
-				SELCTL<='1';
+				
+				-- SELCTL = W1 or W2
+				SELCTL <= '1';
+				-- STOP = W1 or W2
+				STOP_REG <= W1 or W2;
+				
+				-- SEL0 = W1 or W2
 				SEL0<=W1 or W2;
-				STOP_REG<=W1 or W2;
-				SEL3<=W2;
+				-- SEL1 = W2
 				SEL1<=W2;
+				-- SEL2 = 0
+				
+				-- SEL3 = W2
+				SEL3<=W2;
+				
 			when "100" =>
-				SELCTL<='1';
-				SST0<=W2;
-				SBUS<=W1 or W2;
-				STOP_REG<=W1 or W2;
-				DRW<=W1 or W2;
-				SEL3<=(ST0 and W1) or (ST0 and W2);
-				SEL2<=W2;
-				SEL1<=((not ST0) and W1) or (ST0 and W2);
-				SEL0<=W1;
+				
+				-- SELCTL = (ST0=0 or ST0=1) and (W1 or W2)
+				SELCTL <= '1';
+				-- SBUS = (ST0=0 or ST0=1) and (W1 or W2)
+				SBUS <= W1 or W2;
+				-- STOP = (ST0=0 or ST0=1) and (W1 or W2)
+				STOP_REG <= W1 or W2;
+				-- DRW = (ST0=0 or ST0=1) and (W1 or W2)
+				DRW <= W1 or W2;
+				
+				-- SEL0 = (ST0=0 or ST0=1) and W1
+				SEL0 <= W1;
+				-- SEL1 = ((ST0=0) and W1) or ((ST0=1) and W2)
+				SEL1 <= ((not ST0) and W1) or (ST0 and W2);
+				-- SEL2 = (ST0=0 or ST0=1) and W2
+				SEL2 <= W2;
+				-- SEL3 = (ST0=1) and (W1 or W2) 
+				SEL3 <= ST0 and (W1 or W2);
+				
+				-- TODO: 存疑
+				SST0 <= W2;
+				
 			when others=>
 		end case;
 	end process;
